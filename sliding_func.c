260b5d3a9fabf79ba6af49d88df88d2608f2a4fa
MrSigma123 <karol.czolpinski@gmail.com>
2024-07-12 21:08:49 +0200

    Add clear command before displaying logo

--- sliding_func.c
+++ sliding_func.c
@@ -1,15 +1,19 @@
 /* functions definitions for Sliding_puzzle project */
 #include "sliding_func.h"
 #include <stdio.h>
+#include <stdlib.h>
 void logo(void) {
-  printf(" .--. .-.   _    .-. _                   .---.                   .-.        \n");
-  printf(": .--': :  :_;   : ::_;                  : .; :                  : :        \n");
-  printf("`. `. : :  .-. .-' :.-.,-.,-. .--.       :  _.'.-..-..---. .---. : :   .--. \n");
-  printf(" _`, :: :_ : :' .; :: :: ,. :' .; :      : :   : :; :`-'_.'`-'_.': :_ ' '_.'\n");
-  printf("`.__.'`.__;:_;`.__.':_;:_;:_;`._. ; _____:_;   `.__.'`.___;`.___;`.__;`.__.'\n");
-  printf("                              .-. ::_____:                                  \n");
-  printf("                              `._.'                                         \n");
-  printf("\n");
+  system("clear");
+  printf("software version 0.1.0\n");
+  printf("\n\n\n\n\n\n\n\n\n");
+  printf("        .--. .-.   _    .-. _                   .---.                   .-.        \n");
+  printf("       : .--': :  :_;   : ::_;                  : .; :                  : :        \n");
+  printf("       `. `. : :  .-. .-' :.-.,-.,-. .--.       :  _.'.-..-..---. .---. : :   .--. \n");
+  printf("        _`, :: :_ : :' .; :: :: ,. :' .; :      : :   : :; :`-'_.'`-'_.': :_ ' '_.'\n");
+  printf("       `.__.'`.__;:_;`.__.':_;:_;:_;`._. ; _____:_;   `.__.'`.___;`.___;`.__;`.__.'\n");
+  printf("                                     .-. ::_____:                                  \n");
+  printf("                                     `._.'                                         \n");
+  printf("\n\n\n\n\n\n\n\n\n");
 }
 void puzzle8(void) {}
 void puzzle15(void) {}
